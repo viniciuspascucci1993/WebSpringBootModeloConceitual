112b8619221485c395a9d5fbbdae09c4e7e1e41c
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2019-09-23 23:33:56 -0300

    Criando operação de instanciação

--- src/main/java/com/vinicius/springboot/mc/WebSpringBootComercialMcApplication.java
+++ src/main/java/com/vinicius/springboot/mc/WebSpringBootComercialMcApplication.java
@@ -1,13 +1,33 @@
 package com.vinicius.springboot.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WebSpringBootComercialMcApplication {
+import com.vinicius.springboot.mc.model.Categoria;
+import com.vinicius.springboot.mc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class WebSpringBootComercialMcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WebSpringBootComercialMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		
 	}
 
 }
