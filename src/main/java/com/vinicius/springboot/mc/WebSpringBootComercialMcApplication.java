b7b087516656970695b5e6262d718a92ad914b8e
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2019-09-23 21:18:48 -0300

    O projeto foi criado

---
+++ src/main/java/com/vinicius/springboot/mc/WebSpringBootComercialMcApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.springboot.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringBootComercialMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSpringBootComercialMcApplication.class, args);
+	}
+
+}
