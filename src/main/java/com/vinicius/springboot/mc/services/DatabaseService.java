cb95c24d89df7b78725591acd9d123b94a942477
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2019-10-30 22:49:58 -0300

    Acrescentando mais produtos para testar infinity scroll

--- src/main/java/com/vinicius/springboot/mc/services/DatabaseService.java
+++ src/main/java/com/vinicius/springboot/mc/services/DatabaseService.java
@@ -92,10 +92,54 @@ public class DatabaseService {
 		Produto produto10 = new Produto(null, "Calça Jeans Sport", 45.00, 15.00, "Cor Azul", SituacaoProduto.DISPONIVEL);
 		Produto produto11 = new Produto(null, "Samsung Galaxy S10 128GB", 2500.00, 30.00, "Cor Azul", SituacaoProduto.DISPONIVEL);
 		
+		// Jogos Playstation 4
+		Produto produto12 = new Produto(null, "9 Monkeys of Shaolin (Pré-venda) - PS4", 129.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto13 = new Produto(null, "Agents Of Mayhem Day One Edition - Ps4", 64.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto14 = new Produto(null, "Agony - Ps4", 99.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto15 = new Produto(null, "Air Conflicts Double Pack - Ps4", 97.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto16 = new Produto(null, "Airheart Tales of Broken Wings - PS4", 319.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto17 = new Produto(null, "Anthem - PS4", 125.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto18 = new Produto(null, "Assassins Creed Chronicles - Ps4", 89.00, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto19= new Produto(null, "Assassins Creed IV Black Flag - Ps4", 89.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto20 = new Produto(null, "Assassins Creed Rogue Remastered - Ps4", 99.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto21 = new Produto(null, "Assassins Creed Syndicate - Ps4", 94.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto22 = new Produto(null, "Assassins Creed Unity: Limited Edition - Ps4", 59.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto23 = new Produto(null, "Attack On Titan 2 Final Battle - Ps4", 249.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto24 = new Produto(null, "Axiom Verge - Ps4", 94.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto25 = new Produto(null, "Battlefield 1 - Ps4", 74.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto26 = new Produto(null, "Battlefield 1 Revolution (Europeu) - Ps4", 89.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto27 = new Produto(null, "Battlefield 1 Revolution - Ps4", 94.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto28 = new Produto(null, "Battlefield V - Ps4", 139.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto29 = new Produto(null, "Black Mirror - Ps4", 84.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto30 = new Produto(null, "Call Of Duty: Infinite Warfare Legacy Edition - Ps4", 74.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto31 = new Produto(null, "Call of Duty: Modern Warfare - Ps4", 229.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto32 = new Produto(null, "Dark Souls III The Fire Fades Edition - Ps4", 99.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto33 = new Produto(null, "Dark Souls Ii: Scholar Of The First Sin - Ps4", 97.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto34 = new Produto(null, "Dead By Daylight Special Edition - Ps4", 102.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto35 = new Produto(null, "Dead Island 2 (Pré-venda) - Ps4", 219.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto36 = new Produto(null, "Death Stranding (Pré-venda) - PS4", 234.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto37 = new Produto(null, "Detroit Become Human - Ps4", 94.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto38 = new Produto(null, "Devil May Cry Hd Collection - Ps4", 102.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto39 = new Produto(null, "Doom - Ps4", 99.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto40 = new Produto(null, "Doom Eternal (Pré-venda) - PS4", 229.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto41 = new Produto(null, "Dynasty Warriors 9 - Ps4", 209.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto42 = new Produto(null, "Exile's End - Ps4", 239.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto43 = new Produto(null, "Gundam Versus - Ps4", 79.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto44 = new Produto(null, "Hellblade Senua's Sacrifice - Ps4", 149.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto45 = new Produto(null, "Hitman Definitive Edition - Ps4", 99.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto46 = new Produto(null, "Hunting Simulator - Ps4", 129.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto47 = new Produto(null, "Jurassic World Evolution - Ps4", 287.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto48 = new Produto(null, "Immortal Unchained - Ps4", 229.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto49 = new Produto(null, "Just Cause 3 - Ps4", 94.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto50 = new Produto(null, "Just Cause 4 - Ps4", 99.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		Produto produto51 = new Produto(null, "Left Alive - Ps4", 58.99, 05.10, "Cor Azul", SituacaoProduto.DISPONIVEL);
+		
 		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto3));
 		categoria2.getProdutos().addAll(Arrays.asList(produto2, produto4));
 		categoria3.getProdutos().addAll(Arrays.asList(produto5));
-		categoria4.getProdutos().addAll(Arrays.asList(produto6, produto7));
+		categoria4.getProdutos().addAll(Arrays.asList(produto6, produto7, produto12, produto13, produto14, produto15, produto16, produto17, produto18, produto19, produto20, produto21, produto22,
+				produto23, produto24, produto25, produto26, produto27, produto28, produto29, produto30, produto31, produto32, produto33, produto34, produto35, produto36, produto37, produto38, produto39,
+				produto40, produto41, produto42, produto43, produto44, produto45, produto46, produto47, produto48, produto49, produto50, produto51));
 		categoria5.getProdutos().addAll(Arrays.asList(produto8));
 		categoria6.getProdutos().addAll(Arrays.asList(produto9, produto11));
 		categoria7.getProdutos().addAll(Arrays.asList(produto10));
@@ -113,8 +157,52 @@ public class DatabaseService {
 		produto10.getCategorias().addAll(Arrays.asList(categoria7));
 		produto11.getCategorias().addAll(Arrays.asList(categoria6));
 		
+		produto12.getCategorias().addAll(Arrays.asList(categoria4));
+		produto13.getCategorias().addAll(Arrays.asList(categoria4));
+		produto14.getCategorias().addAll(Arrays.asList(categoria4));
+		produto15.getCategorias().addAll(Arrays.asList(categoria4));
+		produto16.getCategorias().addAll(Arrays.asList(categoria4));
+		produto17.getCategorias().addAll(Arrays.asList(categoria4));
+		produto18.getCategorias().addAll(Arrays.asList(categoria4));
+		produto19.getCategorias().addAll(Arrays.asList(categoria4));
+		produto20.getCategorias().addAll(Arrays.asList(categoria4));
+		produto21.getCategorias().addAll(Arrays.asList(categoria4));
+		produto22.getCategorias().addAll(Arrays.asList(categoria4));
+		produto23.getCategorias().addAll(Arrays.asList(categoria4));
+		produto24.getCategorias().addAll(Arrays.asList(categoria4));
+		produto25.getCategorias().addAll(Arrays.asList(categoria4));
+		produto26.getCategorias().addAll(Arrays.asList(categoria4));
+		produto27.getCategorias().addAll(Arrays.asList(categoria4));
+		produto28.getCategorias().addAll(Arrays.asList(categoria4));
+		produto29.getCategorias().addAll(Arrays.asList(categoria4));
+		produto30.getCategorias().addAll(Arrays.asList(categoria4));
+		produto31.getCategorias().addAll(Arrays.asList(categoria4));
+		produto32.getCategorias().addAll(Arrays.asList(categoria4));
+		produto33.getCategorias().addAll(Arrays.asList(categoria4));
+		produto34.getCategorias().addAll(Arrays.asList(categoria4));
+		produto35.getCategorias().addAll(Arrays.asList(categoria4));
+		produto36.getCategorias().addAll(Arrays.asList(categoria4));
+		produto37.getCategorias().addAll(Arrays.asList(categoria4));
+		produto38.getCategorias().addAll(Arrays.asList(categoria4));
+		produto39.getCategorias().addAll(Arrays.asList(categoria4));
+		produto40.getCategorias().addAll(Arrays.asList(categoria4));
+		produto41.getCategorias().addAll(Arrays.asList(categoria4));
+		produto42.getCategorias().addAll(Arrays.asList(categoria4));
+		produto43.getCategorias().addAll(Arrays.asList(categoria4));
+		produto44.getCategorias().addAll(Arrays.asList(categoria4));
+		produto45.getCategorias().addAll(Arrays.asList(categoria4));
+		produto46.getCategorias().addAll(Arrays.asList(categoria4));
+		produto47.getCategorias().addAll(Arrays.asList(categoria4));
+		produto48.getCategorias().addAll(Arrays.asList(categoria4));
+		produto49.getCategorias().addAll(Arrays.asList(categoria4));
+		produto50.getCategorias().addAll(Arrays.asList(categoria4));
+		produto51.getCategorias().addAll(Arrays.asList(categoria4));
+		
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3, categoria4, categoria5, categoria6, categoria7));
-		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3, produto4, produto5, produto6, produto7, produto8, produto9, produto10, produto11));
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3, produto4, produto5, produto6, produto7, produto8, produto9, produto10, produto11,
+				produto12, produto13, produto14, produto15, produto16, produto17, produto18, produto19, produto20, produto21, produto22, produto23, produto24, produto25, produto26, produto27, produto28, produto29,
+				produto30, produto31, produto32, produto33, produto34, produto35, produto36, produto37, produto38, produto39, produto40, produto41, produto32, produto43, produto44, produto45, produto46,
+				produto47, produto48, produto49, produto50, produto51));
 		
 		Estado estado1  = new Estado(null, "Minas-gerais");
 		Estado estado2 = new Estado(null, "São Paulo");
