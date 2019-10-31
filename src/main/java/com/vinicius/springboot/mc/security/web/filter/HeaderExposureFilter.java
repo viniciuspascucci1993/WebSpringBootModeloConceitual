fdfbf8e3b43d868e344ee5e9154246b630bc5e65
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2019-10-30 23:45:53 -0300

    Expondo header location nas respostas

---
+++ src/main/java/com/vinicius/springboot/mc/security/web/filter/HeaderExposureFilter.java
@@ -0,0 +1,41 @@
+package com.vinicius.springboot.mc.security.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Filtro responsável por expor o location no cabeçalho da requisilção.
+ * @author Vinicius-PC - Vinicius Torres Pascucci.
+ */
+@Component
+public class HeaderExposureFilter implements Filter {
+
+	public void init( FilterConfig filterConfig ) throws ServletException {
+		
+	}
+	
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletResponse res = ( HttpServletResponse ) response; 
+		
+		res.addHeader("access-control-expose-headers", "location");
+		
+		chain.doFilter(request, response);
+	}
+	
+	@Override
+	public void destroy() { }
+
+}
